771a5b7b600d36876a5f4921001c66f7e1716e97
deunoh <denovann@live.fr>
2024-09-30 11:27:32 +0200

    font header et dark mode

--- datashot-front/src/components/Header/Header.jsx
+++ datashot-front/src/components/Header/Header.jsx
@@ -1,9 +1,27 @@
+
+import { useState, useEffect } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import './Header.scss';
 
 const Header = () => {
-  return <header>
-    <h1 className='maint-title'>Datashot</h1>
-  </header>
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <header>
+      <h1 className='main-title'>DataShot</h1>
+      <button onClick={toggleDarkMode} className="theme-toggle">
+        {darkMode ? <FaSun /> : <FaMoon />}
+      </button>
+    </header>
+  );
 }
 
 export default Header;
